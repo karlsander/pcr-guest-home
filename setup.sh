80d74fb09e521b09bebf232d0ad86de11eb0e54d
Karl Sander <kall@kall.ws>
2015-07-14 18:58:38 +0200

    fix symlink command in setup script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ apt-get -q -y install git libreoffice unattended-upgrades
 dpkg-reconfigure -plow unattended-upgrades
 
 git clone https://github.com/karlsander/pcr-guest-home
-chmod 777 pcr-guest-home
-ln -s pcr-guest-home /etc/guest-session/skel
+chmod -R 777 pcr-guest-home
+mkdir /etc/guest-session/
+sudo ln -s /home/nbadmin/pcr-guest-home/ /etc/guest-session/skel
 
 printf "[SeatDefaults]\nuser-session=xubuntu\nautologin-user=guest\nautologin-user-timeout=0\nautologin-guest=true\n" > /etc/lightdm/lightdm.conf.d/10-xubuntu.conf
