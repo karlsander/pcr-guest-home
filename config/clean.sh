38650330951b57e425b3db4e7cb1b40cf3e723cf
Karl Sander <kall@kall.ws>
2016-12-18 18:40:31 +0100

    delete repository from machine in cleanup script

--- config/clean.sh
+++ config/clean.sh
@@ -3,4 +3,6 @@ rm -rf /etc/guest-session/
 cat /home/nbadmin/pcr-guest-home/config/10-xubuntu-clean.conf > /etc/lightdm/lightdm.conf.d/10-xubuntu.conf
 cat /home/nbadmin/pcr-guest-home/config/anacrontab-clean > /etc/anacrontab
 
+rm -rf /home/nbadmin/pcr-guest-home
+
 passwd nbadmin
